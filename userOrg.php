2606e4ea32275a2ce7ab5055b4d66a0561b16c02
karthika-vs <karthikavs2005@gmail.com>
2024-11-21 16:56:04 +0530

    final updates

--- userOrg.php
+++ userOrg.php
@@ -14,8 +14,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style5.css">
-  <link rel="stylesheet" href="userOrg.css">
+  <link rel="stylesheet" href="Styeles/style5.css">
+  <link rel="stylesheet" href="Styles/userOrg.css">
 </head>
 <body>
   <div class="all">
